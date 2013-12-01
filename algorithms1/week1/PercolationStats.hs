0fde7115178c469dbe7f22e216ba4ef65570feda
(cdep)illabout <cdep.illabout@gmail.com>
2013-12-01 20:28:22 +0900

    Floundering around trying to get these stats to work.

--- algorithms1/week1/PercolationStats.hs
+++ algorithms1/week1/PercolationStats.hs
@@ -1,4 +1,75 @@
 
-module PercolationStats (
+module PercolationStats ( runPercolationStats
+                        , PercolationStats(..)
                         )
                         where
+
+import Control.Monad.Trans.Class
+
+import Percolation
+
+data PercolationInfo = PercolationInfo
+                 { percolation :: Percolation
+                 , sideLength :: Int
+                 , times :: Int
+                 } deriving (Show)
+
+data PercolationStats = PercolationStats
+                 { percStatsMean :: Double
+                 , percStatsStdDev :: Double
+                 , percStatsConfidenceLow :: Double
+                 , percStatsConfidenceHigh :: Double
+                 , percStatsSideLength :: Int
+                 , percStatsTimes :: Int
+                 } deriving (Show)
+
+
+mean :: undefiend
+mean = undefined
+
+stddev :: undefiend
+stddev = undefined
+
+confidenceLo :: undefiend
+confidenceLo = undefined
+
+confidenceHi :: undefiend
+confidenceHi = undefined
+
+openRandom :: Percolation -> IO Percolation
+openRandom perc = undefined
+
+findWhenPercolates :: Int -> IO Int
+findWhenPercolates sideLength = do
+        let perc = newPercolation sideLength
+        allOpens <- openUntilPercolates perc
+        return $ length allOpens
+  where
+    tryPercs :: (Bool, Percolation) -> IO (Bool, Percolation)
+    tryPercs (doesPerc, perc)
+        | doesPerc == True = error "This should also never percolate!!!"
+        | otherwise = do
+            newPerc <- openRandom perc
+            return (doesPercolate newPerc, newPerc)
+
+    openUntilPercolatesHelper :: (Bool, Percolation) -> [IO (Bool, Percolation)]
+    openUntilPercolatesHelper (doesPerc, perc) = do
+            (newDoesPerc, newPerc) <- (lift $ tryPercs (doesPerc, perc)) :: [IO (Bool, Percolation)]
+            if newDoesPerc == True
+                then [return (True, newPerc)]
+                else return (False, newPerc) : openUntilPercolatesHelper (False, newPerc)
+
+    openUntilPercolates :: Percolation -> IO [(Bool, Percolation)]
+    openUntilPercolates perc
+        | doesPercolate perc = error "It should not percolate from the very beginning!"
+        | otherwise = sequence $ openUntilPercolatesHelper (False, perc)
+
+runPercolationStats :: Int -> Int -> IO PercolationStats
+runPercolationStats n t = do undefined
+
+--        PercolationStats
+ --                       (mean)
+  --                      (stddev)
+   --                     (confidenceLo)
+    --                    (confidenceHi)
+
--- algorithms1/week1/Trash.hs
+++ algorithms1/week1/Trash.hs
@@ -42,8 +42,11 @@ doWhat array = do
         return f
 
 
+-----------------------------------
+-- Weighted Quick Union MAIN!!!! --
+-----------------------------------
 
--- Weighted Quick Union MAIN!!!!
+
 die :: String -> IO a
 die err = do putStrLn $ "ERROR: " ++ err
              exitWith (ExitFailure 1)
@@ -104,3 +107,74 @@ main = do
                     `ap` (maybe (parsingFailure strA) return $ readMaybe strA)
                     `ap` (maybe (parsingFailure strB) return $ readMaybe strB)
                 _ -> die $ "Couldn't parse two integers from line: " ++ line
+
+
+--------------------------
+-- Percolation MAIN!!!! --
+--------------------------
+
+die :: String -> IO a
+die err = do putStrLn $ "ERROR: " ++ err
+             exitWith (ExitFailure 1)
+
+parsingFailure :: String -> IO a
+parsingFailure val = die $ printf "Couldn't parse \"%s\" as an integer" val
+
+parsingFailureWithName :: String -> String -> IO a
+parsingFailureWithName name val =
+        die $ printf "Couldn't parse %s \"%s\" as an integer" name val
+
+main :: IO ()
+main = do
+        --[n, times] <- getArgs
+        args <- getArgs
+        progName <- getProgName
+        case args of
+            [strNums, strTimes] -> do
+                let maybeNums = readMaybe strNums :: Maybe Int
+                    maybeTimes = readMaybe strTimes :: Maybe Int
+                checkMaybeArgs strNums maybeNums strTimes maybeTimes
+            _ -> die ("Must run like `" ++ progName ++ " SIZE TIMES`.")
+    where
+        checkMaybeArgs :: String -> Maybe Int -> String -> Maybe Int -> IO ()
+        checkMaybeArgs badStrNums Nothing _ _ =
+            die ("Could not parse NUMS \"" ++ badStrNums ++ "\" as integer")
+        checkMaybeArgs _ _ badStrTimes Nothing =
+            die ("Could not parse TIMES \"" ++ badStrTimes ++ "\" as integer")
+        checkMaybeArgs _ (Just nums) _ (Just times) = runWithNumsAndTimes nums times
+
+        runWithNumsAndTimes :: Int -> Int -> IO ()
+        runWithNumsAndTimes nums times = do
+            let perc = newPercolation nums
+            _ <- operateOnForever perc helper
+            exitWith ExitSuccess
+
+        operateOnForever :: Monad m => t -> (t -> m t) -> m b
+        operateOnForever value function = do
+            returnValue <- function value
+            operateOnForever returnValue function
+
+        helper :: Percolation -> IO (Percolation)
+        helper perc = do
+                print perc
+                (p, q) <- readTwoInts
+                let pointA = toXY p q
+                newPerc <- if isOpen perc pointA
+                                   then putStrLn "Already open!" >> return perc
+                                   else return $ open perc pointA
+                if isFull newPerc pointA
+                    then putStrLn "Filled with water!!"
+                    else putStrLn "No water yet..."
+                when (doesPercolate perc) (putStrLn "Percolates!!!!!!")
+                return newPerc
+
+        readTwoInts :: IO (Int, Int)
+        readTwoInts = do
+            line <- getLine
+            when (line == "") $ exitWith ExitSuccess
+            case words line of
+                [strA, strB] -> return (,)
+                    `ap` (maybe (parsingFailure strA) return $ readMaybe strA)
+                    `ap` (maybe (parsingFailure strB) return $ readMaybe strB)
+                _ -> die $ "Couldn't parse two integers from line: " ++ line
+
