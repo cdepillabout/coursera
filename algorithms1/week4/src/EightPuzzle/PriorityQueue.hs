8346a25a09e9258bae86087407fda48e36925c51
(cdep)illabout <cdep.illabout@gmail.com>
2014-01-03 03:59:19 +0900

    Make faster.

--- algorithms1/week4/src/EightPuzzle/PriorityQueue.hs
+++ algorithms1/week4/src/EightPuzzle/PriorityQueue.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
 
 module EightPuzzle.PriorityQueue
     ( PriorityQueue
@@ -80,23 +81,48 @@ minOfPQ pqRef = safeReadVec pqRef 0
 insert :: forall s k. (Ord k, Show k) => PriorityQueue s k -> k -> ST s ()
 insert (PQ ref) item = do
         pq <- readSTRef ref
-        debugPrintPQMsg_ "in insert, before inserting and checkResize, " pq
+        --debugPrintPQMsg_ "in insert, before inserting and checkResize, " pq
         pq' <- checkSize =<< insert' pq
-        debugPrintPQMsg_ "in insert, after after inserting and checkResize, " pq'
+        --debugPrintPQMsg_ "in insert, after after inserting and checkResize, " pq'
         writeSTRef ref pq'
-        debug "done insert"
+        --debug "done insert"
   where
     insert' :: PriorityQueue_ s k -> ST s (PriorityQueue_ s k)
     insert' pq@(PriorityQueue vec _ countRef) = do
         count <- readSTRef countRef
         Vector.write vec count item
         modifySTRef countRef (1+)
-        debugPrintPQMsg_ "in insert', before swimming" pq
+        --debugPrintPQMsg_ "in insert', before swimming" pq
         pq' <- swim pq count
-        debugPrintPQMsg_ "in insert', after swimming" pq'
+        --debugPrintPQMsg_ "in insert', after swimming" pq'
         return pq'
 
-delMin = undefined
+delMin :: (Show k, Ord k) => PriorityQueue s k -> ST s (Maybe k)
+delMin pqRef@(PQ ref) = do
+        empty <- isEmpty pqRef
+        if empty
+            then return Nothing
+            else do
+                minFromQueue <- minOfPQ pqRef
+                pq <- readSTRef ref
+                let (PriorityQueue vec _ countRef) = pq
+                count <- readSTRef countRef
+
+                -- exchange the first and last elements
+                pq' <- exchange pq 0 (count - 1)
+                -- write bottom to the previous last element
+                Vector.write vec (count - 1) undefined
+                -- update the count
+                writeSTRef countRef (count -  1)
+                -- sink the new top element to where it should be
+                pq'' <- sink pq' 0 (count - 1)
+
+                -- possible resize the priority queue
+                checkSize pq''
+
+                return minFromQueue
+
+
 
 newRef :: PriorityQueue_ s k -> ST s (PriorityQueue s k)
 newRef pq = fmap PQ $ newSTRef pq
@@ -119,36 +145,42 @@ unsafeReadVec (PQ ref) idx = do
 checkSize :: forall s k. PriorityQueue_ s k -> ST s (PriorityQueue_ s k)
 checkSize pq@(PriorityQueue _ vecSize countRef) = do
         count <- readSTRef countRef
-        debugPrintPQMsg_ "in checkSize (after inserting), " pq
+        --debugPrintPQMsg_ "in checkSize (after inserting or deleting), " pq
         go count
   where
       go :: Int -> ST s (PriorityQueue_ s k)
       go count
             | count == vecSize = do
-                debug "EXPANDING!!!"
+                --debug "EXPANDING!!!"
                 resize pq (vecSize*2)
             | count <= vecSize `div` 4 = do
-                debug "SHRINKING!!!"
+                --debug "SHRINKING!!!"
                 resize pq (vecSize `div` 2)
             | otherwise = do
-                debug "NOT RESIZING!!!"
+                --debug "NOT RESIZING!!!"
                 return pq
 
 resize :: PriorityQueue_ s k -> Int -> ST s (PriorityQueue_ s k)
-resize (PriorityQueue vec vecSize countRef) newSize = do
+resize pq@(PriorityQueue vec vecSize countRef) newSize = do
         newPriorityQueue <- newSizedPQReal newSize
         let (PriorityQueue newVec _ newCountRef) = newPriorityQueue
-        readSTRef countRef >>= writeSTRef newCountRef
-        copyArrayTo newVec
+        count <- readSTRef countRef
+        writeSTRef newCountRef count
+        --debugPrintPQMsg_ "in resize old queue, " pq
+        --debugPrintPQMsg_ "in resize new queue, " newPriorityQueue
+        --copyArrayTo newVec count
+        let oldVecSlice = Vector.unsafeSlice 0 count vec
+        let newVecSlice = Vector.unsafeSlice 0 count newVec
+        Vector.unsafeCopy newVecSlice oldVecSlice
         return newPriorityQueue
   where
-    copyArrayTo newVec = go 0
+    copyArrayTo newVec count = go 0
       where
-        go idx
-            | idx >= vecSize = return ()
+        go !idx
+            | idx >= count = return ()
             | otherwise = do
                 Vector.read vec idx >>= Vector.write newVec idx
-                go (idx+1)
+                go $ idx+1
 
 
 swim :: (Ord k, Show k) => PriorityQueue_ s k -> Int -> ST s (PriorityQueue_ s k)
@@ -157,21 +189,48 @@ swim pq idx
         | otherwise = do
             let parentIdx = ((idx + 1) `div` 2) - 1
             isGreater <- greater pq parentIdx idx
-            debug ("in swim, idx: " ++ show idx ++ ", parentIdx: " ++ show parentIdx)
+            --debug ("in swim, idx: " ++ show idx ++ ", parentIdx: " ++ show parentIdx)
             case isGreater of
                 True -> do
                     pq' <- exchange pq idx parentIdx
                     swim pq' parentIdx
                 False -> return pq
 
+sink :: (Ord k, Show k) => PriorityQueue_ s k -> Int -> Int -> ST s (PriorityQueue_ s k)
+sink pq idx count
+        | (2 * (idx+1)) - 1 >= count = return pq
+        | otherwise = do
+            let j = 2 * idx
+            --debug ("in sink, idx: " ++ show idx ++ ", j: " ++ show j ++ ", count: " ++ show count)
+            isGreater <- greater pq j (j+1)
+            let j' = if j < count && isGreater then (j+1) else j
+            isGreater' <- greater pq idx j'
+            if not isGreater'
+                then return pq
+                else do
+                    pq' <- exchange pq idx j'
+                    sink pq' j' count
+{-
+    while (2*k <= N) {
+        int j = 2*k;
+        if (j < N && greater(j, j+1)) j++;
+        if (!greater(k, j)) break;
+        exch(k, j);
+        k = j;
+    }
+-}
+
+
 exchange :: (Show k) => PriorityQueue_ s k -> Int -> Int -> ST s (PriorityQueue_ s k)
 exchange pq@(PriorityQueue vec _ _) idxA idxB = do
-        itemA <- Vector.read vec idxA
-        itemB <- Vector.read vec idxB
-        debug ("in exch, exching idxA (" ++ show idxA ++ ") with idxB(" ++ show idxB ++ ")")
-        debug ("in exch, exching itemA (" ++ show itemA ++ ") with idxB(" ++ show itemB ++ ")")
-        Vector.write vec idxA itemB
-        Vector.write vec idxB itemA
+        Vector.swap vec idxA idxB
+
+        --itemA <- Vector.read vec idxA
+        --itemB <- Vector.read vec idxB
+        --debug ("in exch, exching idxA (" ++ show idxA ++ ") with idxB(" ++ show idxB ++ ")")
+        --debug ("in exch, exching itemA (" ++ show itemA ++ ") with idxB(" ++ show itemB ++ ")")
+        --Vector.write vec idxA itemB
+        --Vector.write vec idxB itemA
         return pq
 
 greater :: (Ord k) => PriorityQueue_ s k -> Int -> Int -> ST s (Bool)
--- algorithms1/week4/src/Main.hs
+++ algorithms1/week4/src/Main.hs
@@ -9,32 +9,79 @@ import Control.Monad.ST (runST, ST)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Writer (runWriter, tell, runWriterT, Writer, writer, WriterT, execWriterT)
 import Control.Monad.Trans.Class (lift)
+import Control.Monad (forM_)
 
 main :: IO ()
 main = do
-        let result = runST $ execWriterT $ run
+        let result = runST $ execWriterT $ run1
         mapM_ putStrLn result
 
-run :: WriterT [String] (ST s) ()
-run = do
+run2 :: WriterT [String] (ST s) ()
+run2 = do
+        pq <- lift $ newPQ
+        forM_ [1..75000] $ \num -> do
+            lift $ insert pq num
+        forM_ [1..75000] $ \_ -> do
+            lift $ delMin pq
+
+run1 :: WriterT [String] (ST s) ()
+run1 = do
         logMsg "starting..."
         pq <- lift $ newPQ
         logSTOp "new pq, initial size" $ size pq
         lift $ insert pq (3 :: Int)
         logSTOp "after insert 3, size" $ size pq
         logSTOp "after insert 3, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 3, size" $ size pq
+        logSTOp "after delete 3, min" $ minOfPQ pq
         lift $ insert pq 4
         logSTOp "after insert 4, size" $ size pq
         logSTOp "after insert 4, min" $ minOfPQ pq
         lift $ insert pq 2
         logSTOp "after insert 2, size" $ size pq
         logSTOp "after insert 2, min" $ minOfPQ pq
+        lift $ insert pq 5
+        logSTOp "after insert 5, size" $ size pq
+        logSTOp "after insert 5, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 2, size" $ size pq
+        logSTOp "after delete 2, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 4, size" $ size pq
+        logSTOp "after delete 4, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 5, size" $ size pq
+        logSTOp "after delete 5, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete ?, size" $ size pq
+        logSTOp "after delete ?, min" $ minOfPQ pq
+        lift $ insert pq 9
+        logSTOp "after insert 9, size" $ size pq
+        logSTOp "after insert 9, min" $ minOfPQ pq
+        lift $ insert pq 8
+        logSTOp "after insert 8, size" $ size pq
+        logSTOp "after insert 8, min" $ minOfPQ pq
+        lift $ insert pq 7
+        logSTOp "after insert 7, size" $ size pq
+        logSTOp "after insert 7, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 7, size" $ size pq
+        logSTOp "after delete 7, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 8, size" $ size pq
+        logSTOp "after delete 8, min" $ minOfPQ pq
+        lift $ delMin pq
+        logSTOp "after delete 9, size" $ size pq
+        logSTOp "after delete 9, min" $ minOfPQ pq
+        {-
         lift $ insert pq 1
         logSTOp "after insert 1, size" $ size pq
         logSTOp "after insert 1, min" $ minOfPQ pq
         lift $ insert pq 5
         logSTOp "after insert 5, size" $ size pq
         logSTOp "after insert 5, min" $ minOfPQ pq
+        -}
         return ()
   where
       --logSTOp :: (Monad m, Show a) => String -> ST s a -> WriterT [String] (ST s) ()
