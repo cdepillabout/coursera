ab21089c5c6a72cebde1dfeff5c8581f87b34be5
(cdep)illabout <cdep.illabout@gmail.com>
2015-08-14 16:56:13 +0900

    Completed oneVsAll.m

--- machine-learning/machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning/machine-learning-ex3/ex3/oneVsAll.m
@@ -48,17 +48,12 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
-
-
-
-
-
-
-
-
-
-
+all_theta = [];
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+for i = 1:num_labels
+	theta = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
+	all_theta = [all_theta ; theta'];
 
 % =========================================================================
 
