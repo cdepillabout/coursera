fdd75ffea5d581cab1f2895283d3eedf62309a0f
Rashmeet Singh <rashmeet.singh28@gmail.com>
2020-06-23 18:25:23 -0400

    Fix for the "Unrecognized function or variable 'parts'" error.
    **Error message on running the function -**
    Unrecognized function or variable 'parts'.
    Error in submitWithConfiguration (line 4)
      parts = parts(conf);
    Error in submit (line 40)
      submitWithConfiguration(conf);
    
    **Error Description -**
    Following the new changes in Matlab R2019B (Release Notes - https://www.mathworks.com/help/matlab/matlab_prog/upgrade-code-for-r2019b-changes-to-function-precedence-order.html), you cannot use the same name of variable and function. Here, PARTS as variable name overshadows the function.

--- machine-learning/machine-learning-ex4/ex4/lib/submitWithConfiguration.m
+++ machine-learning/machine-learning-ex4/ex4/lib/submitWithConfiguration.m
@@ -1,10 +1,7 @@
 function submitWithConfiguration(conf)
   addpath('./lib/jsonlab');
-
-  parts = parts(conf);
-
+  partsfun = parts(conf);
   fprintf('== Submitting solutions | %s...\n', conf.itemName);
-
   tokenFile = 'token.mat';
   if exist(tokenFile, 'file')
     load(tokenFile);
@@ -12,38 +9,35 @@ function submitWithConfiguration(conf)
   else
     [email token] = promptToken('', '', tokenFile);
   end
-
   if isempty(token)
     fprintf('!! Submission Cancelled\n');
     return
   end
-
   try
-    response = submitParts(conf, email, token, parts);
+    response = submitParts(conf, email, token, partsfun);
   catch
     e = lasterror();
-    fprintf( ...
-      '!! Submission failed: unexpected error: %s\n', ...
-      e.message);
-    fprintf('!! Please try again later.\n');
+    fprintf('\n!! Submission failed: %s\n', e.message);
+    fprintf('\n\nFunction: %s\nFileName: %s\nLineNumber: %d\n', ...
+      e.stack(1,1).name, e.stack(1,1).file, e.stack(1,1).line);
+    fprintf('\nPlease correct your code and resubmit.\n');
     return
   end
-
   if isfield(response, 'errorMessage')
     fprintf('!! Submission failed: %s\n', response.errorMessage);
+  elseif isfield(response, 'errorCode')
+    fprintf('!! Submission failed: %s\n', response.message);
   else
-    showFeedback(parts, response);
+    showFeedback(partsfun, response);
     save(tokenFile, 'email', 'token');
   end
 end
-
 function [email token] = promptToken(email, existingToken, tokenFile)
   if (~isempty(email) && ~isempty(existingToken))
     prompt = sprintf( ...
       'Use token from last successful submission (%s)? (Y/n): ', ...
       email);
     reenter = input(prompt, 's');
-
     if (isempty(reenter) || reenter(1) == 'Y' || reenter(1) == 'y')
       token = existingToken;
       return;
@@ -54,38 +48,32 @@ function [email token] = promptToken(email, existingToken, tokenFile)
   email = input('Login (email address): ', 's');
   token = input('Token: ', 's');
 end
-
 function isValid = isValidPartOptionIndex(partOptions, i)
   isValid = (~isempty(i)) && (1 <= i) && (i <= numel(partOptions));
 end
-
-function response = submitParts(conf, email, token, parts)
-  body = makePostBody(conf, email, token, parts);
-  submissionUrl = submissionUrl();
-  params = {'jsonBody', body};
-  responseBody = urlread(submissionUrl, 'post', params);
-  response = loadjson(responseBody);
+function response = submitParts(conf, email, token, partsfun)
+  body = makePostBody(conf, email, token, partsfun);
+  submissionUrlfun = submissionUrl();
+  responseBody = getResponse(submissionUrlfun, body);
+  jsonResponse = validateResponse(responseBody);
+  response = loadjson(jsonResponse);
 end
-
-function body = makePostBody(conf, email, token, parts)
+function body = makePostBody(conf, email, token, partsfun)
   bodyStruct.assignmentSlug = conf.assignmentSlug;
   bodyStruct.submitterEmail = email;
   bodyStruct.secret = token;
-  bodyStruct.parts = makePartsStruct(conf, parts);
-
+  bodyStruct.parts = makePartsStruct(conf, partsfun);
   opt.Compact = 1;
   body = savejson('', bodyStruct, opt);
 end
-
-function partsStruct = makePartsStruct(conf, parts)
-  for part = parts
+function partsStruct = makePartsStruct(conf, partsfun)
+  for part = partsfun
     partId = part{:}.id;
     fieldName = makeValidFieldName(partId);
     outputStruct.output = conf.output(partId);
     partsStruct.(fieldName) = outputStruct;
   end
 end
-
 function [parts] = parts(conf)
   parts = {};
   for partArray = conf.partArrays
@@ -95,12 +83,11 @@ function [parts] = parts(conf)
     parts{end + 1} = part;
   end
 end
-
-function showFeedback(parts, response)
+function showFeedback(partsfun, response)
   fprintf('== \n');
   fprintf('== %43s | %9s | %-s\n', 'Part Name', 'Score', 'Feedback');
   fprintf('== %43s | %9s | %-s\n', '---------', '-----', '--------');
-  for part = parts
+  for part = partsfun
     score = '';
     partFeedback = '';
     partFeedback = response.partFeedbacks.(makeValidFieldName(part{:}.id));
@@ -114,7 +101,49 @@ function showFeedback(parts, response)
   fprintf('== %43s | %9s | %-s\n', '', totalScore, '');
   fprintf('== \n');
 end
-
+% use urlread or curl to send submit results to the grader and get a response
+function response = getResponse(url, body)
+% try using urlread() and a secure connection
+  params = {'jsonBody', body};
+  [response, success] = urlread(url, 'post', params);
+  if (success == 0)
+    % urlread didn't work, try curl & the peer certificate patch
+    if ispc
+      % testing note: use 'jsonBody =' for a test case
+      json_command = sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, url);
+    else
+      % it's linux/OS X, so use the other form
+      json_command = sprintf('echo ''jsonBody=%s'' | curl -k -X POST -d @- %s', body, url);
+    end
+    % get the response body for the peer certificate patch method
+    [code, response] = system(json_command);
+    % test the success code
+    if (code ~= 0)
+      fprintf('[error] submission with curl() was not successful\n');
+    end
+  end
+end
+% validate the grader's response
+function response = validateResponse(resp)
+  % test if the response is json or an HTML page
+  isJson = length(resp) > 0 && resp(1) == '{';
+  isHtml = findstr(lower(resp), '<html');
+  if (isJson)
+    response = resp;
+  elseif (isHtml)
+    % the response is html, so it's probably an error message
+    printHTMLContents(resp);
+    error('Grader response is an HTML message');
+  else
+    error('Grader sent no response');
+  end
+end
+% parse a HTML response and print it's contents
+function printHTMLContents(response)
+  strippedResponse = regexprep(response, '<[^>]+>', ' ');
+  strippedResponse = regexprep(strippedResponse, '[\t ]+', ' ');
+  fprintf(strippedResponse);
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Service configuration
